65d0f09d8cb8d1bdf7d20ebe2bb9d577b0f143ea
Danil Ilinykh <woodroof@gmail.com>
2016-10-16 14:53:11 +0700

    Добавлен подсчёт времени выполнения теста, вывод теперь более-менее соответствует gtest'у Функция test.run_all_tests теперь возвращает boolean, чтобы её можно было использовать в каких-нибудь скриптах

--- db/test/run_all_tests().sql
+++ db/test/run_all_tests().sql
@@ -3,31 +3,25 @@
 -- DROP FUNCTION test.run_all_tests();
 
 CREATE OR REPLACE FUNCTION test.run_all_tests()
-  RETURNS text AS
+  RETURNS boolean AS
 $BODY$
 -- Тесты запускаются в пустой базе
 -- Ищутся и запускаются функции *_test.*, возвращающие void и не имеющие входных параметров
 -- Тест считается успешно выполненным, если он не выбросил исключения
 declare
-  v_test_case record;
-  v_test record;
-
-  v_test_cases_count integer;
-  v_tests_count integer;
+  v_total_test_cases_count integer;
   v_total_tests_count integer;
-  v_failed_count integer := 0;
 
-  v_current_test_case integer := 1;
-  v_current_test integer;
+  v_total_tests_text text;
+  v_total_test_cases_text text;
 
-  v_void_id integer;
+  v_failed_tests text[];
 
-  v_exception_message text;
-  v_exception_call_stack text;
+  v_start timestamp with time zone := clock_timestamp();
 begin
   -- Определяем количество тест-кейсов
   select count(1)
-  into v_test_cases_count
+  into v_total_test_cases_count
   from pg_namespace
   where nspname like '%_test';
 
@@ -50,91 +44,184 @@ begin
       ) and
     pronargs = 0;
 
-  -- Определяем id типа void для пропуска функций, возвращающих какое-то значение
-  select oid
-  into v_void_id
-  from pg_type
-  where typname = 'void';
+  if v_total_tests_count = 1 then
+    v_total_tests_text := '1 test';
+  else
+    v_total_tests_text := v_total_tests_count || ' tests';
+  end if;
 
-  for v_test_case in
-  (
-    select oid as id, nspname as name
-    from pg_namespace
-    where nspname like '%_test'
-    order by name
-  )
-  loop
-    raise notice 'Running test case % of %: %', v_current_test_case, v_test_cases_count, v_test_case.name;
-    v_current_test_case := v_current_test_case + 1;
+  if v_total_test_cases_count = 1 then
+    v_total_test_cases_text := '1 test case';
+  else
+    v_total_test_cases_text := v_total_test_cases_count || ' test cases';
+  end if;
 
-    v_current_test := 1;
+  raise notice '[==========] Running % from %.', v_total_tests_text, v_total_test_cases_text;
 
-    -- Считаем количество тестов в тест-кейсе
-    select count(1)
-    into v_tests_count
-    from pg_proc
-    where
-      pronamespace = v_test_case.id and
-      prorettype = v_void_id and
-      pronargs = 0;
+  declare
+    v_void_type_id integer;
 
-    for v_test in
+    v_test_case record;
+    v_test record;
+
+    v_tests_count integer;
+    v_tests_text text;
+
+    v_test_name text;
+
+    v_test_case_start timestamp with time zone;
+
+    v_test_start timestamp with time zone;
+    v_test_time integer;
+
+    v_failed boolean;
+  begin
+    -- Определяем id типа void для пропуска функций, возвращающих какое-то значение
+    select oid
+    into v_void_type_id
+    from pg_type
+    where typname = 'void';
+
+    for v_test_case in
     (
-      select
-        proname as name,
-        prorettype as type,
-        pronargs as arg_count,
-        provolatile as volative
-      from pg_proc
-      where pronamespace = v_test_case.id
+      select oid as id, nspname as name
+      from pg_namespace
+      where nspname like '%_test'
       order by name
     )
     loop
-      if v_test.type != v_void_id then
-        raise notice 'Skipping function % due to non-void return value', v_test_case.name || '.' || v_test.name;
-        continue;
+      -- Считаем количество тестов в тест-кейсе
+      select count(1)
+      into v_tests_count
+      from pg_proc
+      where
+        pronamespace = v_test_case.id and
+        prorettype = v_void_type_id and
+        pronargs = 0;
+
+      if v_tests_count = 1 then
+        v_tests_text := '1 test';
+      else
+        v_tests_text := v_tests_count || ' tests';
       end if;
 
-      if v_test.arg_count != 0 then
-        raise notice 'Skipping function % due to more than zero arguments', v_test_case.name || '.' || v_test.name;
-        continue;
-      end if;
+      v_test_case_start := clock_timestamp();
 
-      raise notice 'Running test % of %: %', v_current_test, v_tests_count, v_test.name;
-      v_current_test := v_current_test + 1;
+      raise notice '[----------] % from %', v_tests_text, v_test_case.name;
 
-      begin
+      for v_test in
+      (
+        select
+          proname as name,
+          prorettype as type,
+          pronargs as arg_count,
+          provolatile as volative
+        from pg_proc
+        where pronamespace = v_test_case.id
+        order by name
+      )
+      loop
+        v_test_name := v_test_case.name || '.' || v_test.name;
+
+        if v_test.type != v_void_type_id then
+          raise notice ' SKIPPING FUNCTION % DUE TO NON-VOID RETURN VALUE', v_test_name;
+          continue;
+        end if;
+
+        if v_test.arg_count != 0 then
+          raise notice ' SKIPPING FUNCTION % DUE TO MORE THAN ZERO ARGUMENTS', v_test_name;
+          continue;
+        end if;
+
+        v_test_start := clock_timestamp();
+
+        raise notice '[ RUN      ] %', v_test_name;
+
         begin
-          -- Выполняем процедуру
-          execute 'select ' || v_test_case.name || '.' || v_test.name || '()';
+          declare
+            v_exception_call_stack text;
+            v_exception_message text;
+          begin
+            -- Выполняем процедуру
+            execute 'select ' || v_test_name || '()';
+
+            v_failed := false;
+          exception when others then
+            -- Выводим сообщение об ошибке
+            get stacked diagnostics
+              v_exception_message = message_text,
+              v_exception_call_stack = pg_exception_context;
+
+            raise notice E'%\n%', v_exception_call_stack, v_exception_message;
+            v_failed := true;
+
+            v_failed_tests := array_append(v_failed_tests, v_test_name);
+          end;
+
+          -- Изменения тестов, которые могли менять содержимое БД, нужно откатить
+          if v_test.volative = 'v' then
+            raise exception 'Rollback';
+          end if;
         exception when others then
-          -- Выводим сообщение об ошибке
-          get stacked diagnostics
-            v_exception_message = message_text,
-            v_exception_call_stack = pg_exception_context;
-
-          raise notice 'Test failed!';
-          raise notice E'Error: %\nCall stack:\n%', v_exception_message, v_exception_call_stack;
-
-          v_failed_count := v_failed_count + 1;
         end;
 
-        if v_test.volative = 'v' then
-          -- Откатываем изменения теста
-          raise exception 'Rollback';
-        end if;
-      exception when others then
-      end;
-    end loop;
-  end loop;
+        v_test_time := round(extract(milliseconds from clock_timestamp() - v_test_start));
 
-  if v_failed_count = 0 then
-    raise notice 'Successfully completed % tests in % test cases', v_total_tests_count, v_test_cases_count;
-    return '[OK]';
-  else
-    raise notice 'Failed % out of % tests in % test cases', v_failed_count, v_total_tests_count, v_test_cases_count;
-    return '[FAILED]';
-  end if;
+        if v_failed then
+          raise notice '[  FAILED  ] % (% ms total)', v_test_name, v_test_time;
+        else
+          raise notice '[       OK ] % (% ms total)', v_test_name, v_test_time;
+        end if;
+      end loop;
+    end loop;
+
+    raise notice '[----------] % from % (% ms total)', v_tests_text, v_test_case.name, round(extract(milliseconds from clock_timestamp() - v_test_case_start));
+    raise notice '';
+  end;
+
+  raise notice '[==========] % from % ran. (% ms total)', v_total_tests_text, v_total_test_cases_text, round(extract(milliseconds from clock_timestamp() - v_start));
+
+  declare
+    v_failed_tests_count integer := coalesce(array_length(v_failed_tests, 1), 0);
+    v_passed_tests_count integer := v_total_tests_count - v_failed_tests_count;
+    v_passed_tests_text text;
+  begin
+    if v_passed_tests_count = 1 then
+      v_passed_tests_text := '1 test';
+    else
+      v_passed_tests_text := v_passed_tests_count || ' tests';
+    end if;
+
+    raise notice '[  PASSED  ] %.', v_passed_tests_text;
+
+    if v_failed_tests_count != 0 then
+      declare
+        v_failed_tests_text text;
+        v_failed_test text;
+      begin
+        if v_failed_tests_count = 1 then
+          v_failed_tests_text := 'test';
+        else
+          v_failed_tests_text := 'tests';
+        end if;
+
+        raise notice '[  FAILED  ] % %, listed below:', v_failed_tests_count, v_failed_tests_text;
+
+        foreach v_failed_test in array v_failed_tests loop
+          raise notice '[  FAILED  ] %', v_failed_test;
+        end loop;
+
+        raise notice '';
+        raise notice ' % FAILED %', v_failed_tests_count, upper(v_failed_tests_text);
+      end;
+    end if;
+
+    if v_failed_tests_count != 0 then
+      return false;
+    end if;
+  end;
+
+  return true;
 end;
 $BODY$
   LANGUAGE plpgsql VOLATILE
