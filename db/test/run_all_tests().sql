e32b2b3f7fc5d2e732e3642e0fe3c3ece5656903
Danil Ilinykh <woodroof@gmail.com>
2016-11-02 19:17:14 +0700

    Поддержка disabled-тестов Поддержка функций инициализации тест кейсов (set_up_test_case)

--- db/test/run_all_tests().sql
+++ db/test/run_all_tests().sql
@@ -42,7 +42,9 @@ begin
         from pg_type
         where typname = 'void'
       ) and
-    pronargs = 0;
+    pronargs = 0 and
+    proname not like 'disabled_%' and
+    proname != 'set_up_test_case';
 
   if v_total_tests_count = 1 then
     v_total_tests_text := '1 test';
@@ -62,19 +64,11 @@ begin
     v_void_type_id integer;
 
     v_test_case record;
-    v_test record;
 
     v_tests_count integer;
     v_tests_text text;
 
-    v_test_name text;
-
     v_test_case_start timestamp with time zone;
-
-    v_test_start timestamp with time zone;
-    v_test_time integer;
-
-    v_failed boolean;
   begin
     -- Определяем id типа void для пропуска функций, возвращающих какое-то значение
     select oid
@@ -97,7 +91,9 @@ begin
       where
         pronamespace = v_test_case.id and
         prorettype = v_void_type_id and
-        pronargs = 0;
+        pronargs = 0 and
+        proname not like 'disabled_%' and
+        proname != 'set_up_test_case';
 
       if v_tests_count = 1 then
         v_tests_text := '1 test';
@@ -109,70 +105,159 @@ begin
 
       raise notice '[----------] % from %', v_tests_text, v_test_case.name;
 
-      for v_test in
-      (
-        select
-          proname as name,
-          prorettype as type,
-          pronargs as arg_count,
-          provolatile as volative
-        from pg_proc
-        where pronamespace = v_test_case.id
-        order by name
-      )
-      loop
-        v_test_name := v_test_case.name || '.' || v_test.name;
+      declare
+        v_test record;
+        v_set_up record;
+        v_need_tear_down boolean := false;
 
-        if v_test.type != v_void_type_id then
-          raise notice ' SKIPPING FUNCTION % DUE TO NON-VOID RETURN VALUE', v_test_name;
-          continue;
-        end if;
+        v_test_name text;
+        v_test_start timestamp with time zone;
+        v_test_time integer;
 
-        if v_test.arg_count != 0 then
-          raise notice ' SKIPPING FUNCTION % DUE TO MORE THAN ZERO ARGUMENTS', v_test_name;
-          continue;
-        end if;
+        v_failed boolean;
+      begin
+        for v_set_up in
+        (
+          select
+            proname as name,
+            prorettype as type,
+            pronargs as arg_count,
+            provolatile as volative
+          from pg_proc
+          where
+            pronamespace = v_test_case.id and
+            proname = 'set_up_test_case'
+        )
+        loop
+          if v_set_up.type != v_void_type_id then
+            raise notice ' SKIPPING SET UP FUNCTION FOR TEST CASE % DUE TO NON-VOID RETURN VALUE', v_test_case.name;
+            continue;
+          end if;
 
-        v_test_start := clock_timestamp();
+          if v_set_up.arg_count != 0 then
+            raise notice ' SKIPPING SET UP FUNCTION FOR TEST CASE % DUE TO MORE THAN ZERO ARGUMENTS', v_test_case.name;
+            continue;
+          end if;
 
-        raise notice '[ RUN      ] %', v_test_name;
+          if v_set_up.volative != 'v' then
+            raise notice ' SKIPPING NON-VOLATILE SET UP FUNCTION FOR TEST CASE %', v_test_case.name;
+            continue;
+          end if;
 
-        begin
+          v_need_tear_down := true;
+        end loop;
+
+        if v_need_tear_down then
           declare
             v_exception_call_stack text;
             v_exception_message text;
           begin
-            -- Выполняем процедуру
-            execute 'select ' || v_test_name || '()';
+            execute 'select ' || v_test_case.name || '.set_up_test_case()';
 
-            v_failed := false;
           exception when others then
             -- Выводим сообщение об ошибке
             get stacked diagnostics
               v_exception_message = message_text,
               v_exception_call_stack = pg_exception_context;
 
-            raise notice E'%\n%', v_exception_call_stack, v_exception_message;
-            v_failed := true;
+            raise notice E' TEST CASE SET UP FAILED FOR %\n%\n%', v_test_case.name, v_exception_call_stack, v_exception_message;
 
-            v_failed_tests := array_append(v_failed_tests, v_test_name);
+            for v_test in
+            (
+              select proname as name
+              from pg_proc
+              where
+                pronamespace = v_test_case.id and
+                prorettype = v_void_type_id and
+                pronargs = 0 and
+                proname not like 'disabled_%' and
+                proname != 'set_up_test_case'
+              order by name
+            )
+            loop
+              v_test_name := v_test_case.name || '.' || v_test.name;
+
+              v_failed_tests := array_append(v_failed_tests, v_test_name);
+            end loop;
+
+            continue;
+          end;
+        end if;
+
+        for v_test in
+        (
+          select
+            proname as name,
+            prorettype as type,
+            pronargs as arg_count,
+            provolatile as volative
+          from pg_proc
+          where
+            pronamespace = v_test_case.id and
+            proname not like 'disabled_%' and
+            proname != 'set_up_test_case'
+          order by name
+        )
+        loop
+          v_test_name := v_test_case.name || '.' || v_test.name;
+
+          if v_test.type != v_void_type_id then
+            raise notice ' SKIPPING FUNCTION % DUE TO NON-VOID RETURN VALUE', v_test_name;
+            continue;
+          end if;
+
+          if v_test.arg_count != 0 then
+            raise notice ' SKIPPING FUNCTION % DUE TO MORE THAN ZERO ARGUMENTS', v_test_name;
+            continue;
+          end if;
+
+          v_test_start := clock_timestamp();
+
+          raise notice '[ RUN      ] %', v_test_name;
+
+          begin
+            declare
+              v_exception_call_stack text;
+              v_exception_message text;
+            begin
+              -- Выполняем процедуру
+              execute 'select ' || v_test_name || '()';
+
+              v_failed := false;
+            exception when others then
+              -- Выводим сообщение об ошибке
+              get stacked diagnostics
+                v_exception_message = message_text,
+                v_exception_call_stack = pg_exception_context;
+
+              raise notice E'%\n%', v_exception_call_stack, v_exception_message;
+              v_failed := true;
+
+              v_failed_tests := array_append(v_failed_tests, v_test_name);
+            end;
+
+            -- Изменения тестов, которые могли менять содержимое БД, нужно откатить
+            if !v_failed and v_test.volative = 'v' then
+              raise exception 'Rollback';
+            end if;
+          exception when others then
           end;
 
-          -- Изменения тестов, которые могли менять содержимое БД, нужно откатить
-          if v_test.volative = 'v' then
-            raise exception 'Rollback';
+          v_test_time := round(extract(milliseconds from clock_timestamp() - v_test_start));
+
+          if v_failed then
+            raise notice '[  FAILED  ] % (% ms total)', v_test_name, v_test_time;
+          else
+            raise notice '[       OK ] % (% ms total)', v_test_name, v_test_time;
           end if;
-        exception when others then
-        end;
+        end loop;
 
-        v_test_time := round(extract(milliseconds from clock_timestamp() - v_test_start));
-
-        if v_failed then
-          raise notice '[  FAILED  ] % (% ms total)', v_test_name, v_test_time;
-        else
-          raise notice '[       OK ] % (% ms total)', v_test_name, v_test_time;
+        -- Откатываем изменения тест кейса, если была его инициализация
+        if v_need_tear_down then
+          raise exception 'Rollback';
         end if;
-      end loop;
+      exception when others then
+      end;
 
       raise notice '[----------] % from % (% ms total)', v_tests_text, v_test_case.name, round(extract(milliseconds from clock_timestamp() - v_test_case_start));
       raise notice '';
@@ -182,6 +267,7 @@ begin
   raise notice '[==========] % from % ran. (% ms total)', v_total_tests_text, v_total_test_cases_text, round(extract(milliseconds from clock_timestamp() - v_start));
 
   declare
+    v_disabled_tests_count integer;
     v_failed_tests_count integer := coalesce(array_length(v_failed_tests, 1), 0);
     v_passed_tests_count integer := v_total_tests_count - v_failed_tests_count;
     v_passed_tests_text text;
@@ -213,6 +299,43 @@ begin
 
         raise notice '';
         raise notice ' % FAILED %', v_failed_tests_count, upper(v_failed_tests_text);
+      end;
+    end if;
+
+    select count(1)
+    into v_disabled_tests_count
+    from pg_proc
+    where
+      pronamespace in
+        (
+          select oid
+          from pg_namespace
+          where nspname like '%_test'
+        ) and
+      prorettype =
+        (
+          select oid
+          from pg_type
+          where typname = 'void'
+        ) and
+      pronargs = 0 and
+      proname like 'disabled_%';
+
+    if v_disabled_tests_count != 0 then
+      declare
+        v_disabled_tests_text text;
+      begin
+        if v_failed_tests_count = 0 then
+          raise notice '';
+        end if;
+
+        if v_disabled_tests_count = 1 then
+          v_disabled_tests_text := 'DISABLED TEST';
+        else
+          v_disabled_tests_text := 'DISABLED TESTS';
+        end if;
+
+        raise notice '  YOU HAVE % %', v_disabled_tests_count, v_disabled_tests_text;
       end;
     end if;
 
