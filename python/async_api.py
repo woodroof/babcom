75dd1f162003491094f7aba2c5cfa96d91b1da40
Danil Ilinykh <woodroof@gmail.com>
2018-03-25 18:05:23 +0700

    Добавлена асинхронная python-прослойка

---
+++ python/async_api.py
@@ -0,0 +1,35 @@
+import asyncio
+import asyncpg
+
+from aiohttp import web
+
+DB_NAME = 'babcom'
+DB_USER = 'http'
+DB_PASSWORD = 'http'
+DB_HOST = 'localhost'
+DB_PORT = 5433
+
+PORT = 8000
+
+class InvalidContentTypeError(Exception):
+	pass
+
+async def process_post_request(request):
+	try:
+		content_type = request.headers['Content-Type']
+		if content_type != 'application/json':
+			raise InvalidContentTypeError()
+	except:
+		return web.Response(status=415)
+
+	connection = await asyncpg.connect(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+	try:
+		result = await connection.fetchrow('select * from api.api($1)', await request.text())
+	except asyncpg.exceptions.InvalidTextRepresentationError:
+		return web.Response(status=400)
+
+	return web.Response(status=result[0], text=result[1])
+
+app = web.Application()
+app.router.add_post('/', process_post_request)
+web.run_app(app, port = PORT)
