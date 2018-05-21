b70867c020fcc46f442c129831517c75c3d980a2
Danil Ilinykh <woodroof@gmail.com>
2018-05-21 23:45:45 +0700

    async_api.py переделан на вариант с pool'ом подключений (работает на порядок быстрее!) Кроме того, в предыдущем варианте забыл закрывать подключения :)

--- python/async_api.py
+++ python/async_api.py
@@ -15,14 +15,22 @@ async def process_post_request(request):
 	if request.content_type != 'application/json':
 		return web.Response(status=415)
 
-	connection = await asyncpg.connect(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
-	try:
-		result = await connection.fetchrow('select * from api.api($1)', await request.text())
-	except asyncpg.exceptions.InvalidTextRepresentationError:
-		return web.Response(status=400)
+	pool = request.app['pool']
 
-	return web.Response(status=result[0], content_type='application/json', text=result[1])
+	async with pool.acquire() as connection:
+		try:
+			result = await connection.fetchrow('select * from api.api($1)', await request.text())
+		except asyncpg.exceptions.InvalidTextRepresentationError:
+			return web.Response(status=400)
 
-app = web.Application()
-app.router.add_post('/', process_post_request)
+		return web.Response(status=result[0], content_type='application/json', text=result[1])
+
+async def init_app():
+	app = web.Application()
+	app['pool'] = await asyncpg.create_pool(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+	app.router.add_post('/', process_post_request)
+	return app
+
+loop = asyncio.get_event_loop()
+app = loop.run_until_complete(init_app())
 web.run_app(app, port = PORT)
