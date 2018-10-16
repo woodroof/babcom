01f6598458a8d5e2bac7fe8abb7e993778f7e9cb
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2018-10-16 22:59:47 +0700

    Поддержка CORS

--- python/async_api.py
+++ python/async_api.py
@@ -1,13 +1,14 @@
 import asyncio
 import asyncpg
+import collections
 
 from aiohttp import web
 
-DB_NAME = 'babcom'
-DB_USER = 'http'
-DB_PASSWORD = 'http'
+DB_NAME = 'test'
+DB_USER = 'woodroof'
+DB_PASSWORD = ''
 DB_HOST = 'localhost'
-DB_PORT = 5433
+DB_PORT = 5432
 
 PORT = 8000
 
@@ -23,12 +24,16 @@ async def process_post_request(request):
 		except asyncpg.exceptions.InvalidTextRepresentationError:
 			return web.Response(status=400)
 
-		return web.Response(status=result[0], content_type='application/json', text=result[1])
+		return web.Response(status=result[0], content_type='application/json', text=result[1], headers={'Access-Control-Allow-Origin': '*'})
+
+async def process_options_request(request):
+	return web.Response(status=200, headers={'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'})
 
 async def init_app():
 	app = web.Application()
 	app['pool'] = await asyncpg.create_pool(host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
 	app.router.add_post('/', process_post_request)
+	app.router.add_options('/', process_options_request)
 	return app
 
 loop = asyncio.get_event_loop()
