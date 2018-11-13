d42f67eef5dbe943e3acc5eed67c296a9922b41d
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2018-11-13 21:14:55 +0700

    Добавлена таблица атрибутов

--- db/recreate.sql
+++ db/recreate.sql
@@ -4670,6 +4670,23 @@ language 'plpgsql';
 
 -- Creating tables
 
+-- drop table data.attributes;
+
+create table data.attributes(
+  id integer not null generated always as identity,
+  code text not null,
+  name text,
+  description text,
+  type data.attribute_type not null,
+  card_type data.card_type,
+  value_description_function text,
+  constraint attributes_pk primary key(id),
+  constraint attributes_unique_code unique(code)
+);
+
+comment on column data.attributes.card_type is 'Если null, то применимо ко всем типам карточек';
+comment on column data.attributes.value_description_function is 'Имя функции из схемы attribute_value_description_functions. Функция вызывается с параметрами (user_object_id, attribute_id, value).';
+
 -- drop table data.connections;
 
 create table data.connections(
