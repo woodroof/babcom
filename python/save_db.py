53768dc8fe9d279921503a7c7cc5b468019dd9f0
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2018-11-16 09:35:33 +0700

    Ограничения check сохраняем без лишних скобок

--- python/save_db.py
+++ python/save_db.py
@@ -308,7 +308,7 @@ select
       attnum > 0
   ) as columns,
   (
-    select array_agg(c.conname || ' ' || pg_get_constraintdef(c.oid) order by c.conname)
+    select array_agg(c.conname || ' ' || (case when c.conbin is null then pg_get_constraintdef(c.oid) else 'check' || c.consrc end) order by c.conname)
     from pg_constraint c
     where conrelid = t.oid
   ) as constraints,
