137d9e4aa19ffd8e8c3e704a74cfa16609f0d554
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2019-01-03 14:11:17 +0700

    Добавлено создание основных атрибутов, не зависящих от проекта

---
+++ db/data/init().sql
@@ -0,0 +1,73 @@
+-- drop function data.init();
+
+create or replace function data.init()
+returns void
+volatile
+as
+$$
+begin
+  insert into data.attributes(code, name, description, type, card_type, value_description_function, can_be_overridden) values
+  (
+    'actions_function',
+    null,
+    'Функция, вызываемая перед получением действий объекта, string. Вызывается с параметрами (object_id, actor_id) и возвращает действия.',
+    'system',
+    null,
+    null,
+    false
+  ),
+  (
+    'actor_function',
+    null,
+    'Функция, вызываемая перед получением заголовка и подзаголовка актора, string. Вызывается с параметром (object_id).',
+    'system',
+    null,
+    null,
+    false
+  ),
+  ('content', null, 'Массив идентификаторов объектов списка, integer[]', 'hidden', 'full', null, false),
+  (
+    'full_card_function',
+    null,
+    'Функция, вызываемая перед получением полной карточки объекта, string. Вызывается с параметрами  (object_id, actor_id).',
+    'system',
+    null,
+    null,
+    false
+  ),
+  ('is_visible', null, 'Определяет, доступен ли объект текущему актору, boolean', 'system', null, null, true),
+  (
+    'list_actions_function',
+    null,
+    'Функция, вызываемая перед получением действий объекта списка, string. Вызывается с параметрами (object_id, list_object_id, actor_id) и возвращает действия.',
+    'system',
+    null,
+    null,
+    false
+  ),
+  (
+    'mini_card_function',
+    null,
+    'Функция, вызываемая перед получением миникарточки объекта, string. Вызывается с параметрами (object_id, actor_id).',
+    'system',
+    null,
+    null,
+    false
+  ),
+  (
+    'priority',
+    null,
+    'Приоритет группы, integer. Для стабильной работы приоритет всех групп (объектов, включающих другие объекты) должен быть уникальным. Значение приоритета по умолчанию - 0.',
+    'system',
+    null,
+    null,
+    false
+  ),
+  ('redirect', null, 'Содержит идентификатор объекта, который должен быть возвращён вместо запрошенного при получении полной карточки объекта, integer.', 'system', null, null, true),
+  ('subtitle', null, 'Подзаголовок, string', 'normal', null, null, true),
+  ('temporary_object', null, 'Атрибут, наличие которого говорит о том, что открытый объект не нужно сохранять в истории', 'hidden', 'full', null, false),
+  ('title', null, 'Заголовок, string', 'normal', null, null, true),
+  ('type', null, 'Тип объекта, string', 'hidden', null, null, true);
+end;
+$$
+language 'plpgsql';
--- db/recreate.sql
+++ db/recreate.sql
@@ -1428,6 +1428,80 @@ end;
 $$
 language 'plpgsql';
 
+-- drop function data.init();
+
+create or replace function data.init()
+returns void
+volatile
+as
+$$
+begin
+  insert into data.attributes(code, name, description, type, card_type, value_description_function, can_be_overridden) values
+  (
+    'actions_function',
+    null,
+    'Функция, вызываемая перед получением действий объекта, string. Вызывается с параметрами (object_id, actor_id) и возвращает действия.',
+    'system',
+    null,
+    null,
+    false
+  ),
+  (
+    'actor_function',
+    null,
+    'Функция, вызываемая перед получением заголовка и подзаголовка актора, string. Вызывается с параметром (object_id).',
+    'system',
+    null,
+    null,
+    false
+  ),
+  ('content', null, 'Массив идентификаторов объектов списка, integer[]', 'hidden', 'full', null, false),
+  (
+    'full_card_function',
+    null,
+    'Функция, вызываемая перед получением полной карточки объекта, string. Вызывается с параметрами  (object_id, actor_id).',
+    'system',
+    null,
+    null,
+    false
+  ),
+  ('is_visible', null, 'Определяет, доступен ли объект текущему актору, boolean', 'system', null, null, true),
+  (
+    'list_actions_function',
+    null,
+    'Функция, вызываемая перед получением действий объекта списка, string. Вызывается с параметрами (object_id, list_object_id, actor_id) и возвращает действия.',
+    'system',
+    null,
+    null,
+    false
+  ),
+  (
+    'mini_card_function',
+    null,
+    'Функция, вызываемая перед получением миникарточки объекта, string. Вызывается с параметрами (object_id, actor_id).',
+    'system',
+    null,
+    null,
+    false
+  ),
+  (
+    'priority',
+    null,
+    'Приоритет группы, integer. Для стабильной работы приоритет всех групп (объектов, включающих другие объекты) должен быть уникальным. Значение приоритета по умолчанию - 0.',
+    'system',
+    null,
+    null,
+    false
+  ),
+  ('redirect', null, 'Содержит идентификатор объекта, который должен быть возвращён вместо запрошенного при получении полной карточки объекта, integer.', 'system', null, null, true),
+  ('subtitle', null, 'Подзаголовок, string', 'normal', null, null, true),
+  ('temporary_object', null, 'Атрибут, наличие которого говорит о том, что открытый объект не нужно сохранять в истории', 'hidden', 'full', null, false),
+  ('title', null, 'Заголовок, string', 'normal', null, null, true),
+  ('type', null, 'Тип объекта, string', 'hidden', null, null, true);
+end;
+$$
+language 'plpgsql';
+
 -- drop function data.is_system_attribute(integer);
 
 create or replace function data.is_system_attribute(in_attribute_id integer)
@@ -6289,3 +6363,4 @@ execute function data.objects_after_insert();
 
 -- Initial data
 
+select data.init();
