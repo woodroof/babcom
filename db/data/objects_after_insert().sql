3a4c288990754c51b08bbf05929d5644cf5132c6
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2019-01-19 00:03:04 +0700

    Триггер теперь позволяет вставлять классы в таблицу :)

--- db/data/objects_after_insert().sql
+++ db/data/objects_after_insert().sql
@@ -6,8 +6,10 @@ volatile
 as
 $$
 begin
-  insert into data.object_objects(parent_object_id, object_id)
-  values(new.id, new.id);
+  if new.type = 'instance' then
+    insert into data.object_objects(parent_object_id, object_id)
+    values(new.id, new.id);
+  end if;
 
   return null;
 end;
