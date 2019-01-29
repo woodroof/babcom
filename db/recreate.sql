1ab9362cbb6d6ad2c418642bedee48a681ab0538
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2019-01-30 00:23:43 +0700

    Проверка на уникальность индекса объекта в подписке теперь отложенная

--- db/recreate.sql
+++ db/recreate.sql
@@ -13300,7 +13300,7 @@ create table data.client_subscription_objects(
   constraint client_subscription_objects_index_check check(index > 0),
   constraint client_subscription_objects_object_check check(data.is_instance(object_id)),
   constraint client_subscription_objects_pk primary key(id),
-  constraint client_subscription_objects_unique_csi_i unique(client_subscription_id, index),
+  constraint client_subscription_objects_unique_csi_i unique(client_subscription_id, index) deferrable,
   constraint client_subscription_objects_unique_oi_csi unique(object_id, client_subscription_id)
 );
 
