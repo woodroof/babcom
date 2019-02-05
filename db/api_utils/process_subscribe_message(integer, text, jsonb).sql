6678792ffa151203d10cab16d989a575c037233f
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2019-02-05 23:55:07 +0700

    Теперь redirect'ы всегда возвращаются в виде action open_object

--- db/api_utils/process_subscribe_message(integer, text, jsonb).sql
+++ db/api_utils/process_subscribe_message(integer, text, jsonb).sql
@@ -7,9 +7,8 @@ as
 $$
 declare
   v_object_code text := json.get_string(in_message, 'object_id');
+  v_actor_id integer := data.get_active_actor_id(in_client_id);
   v_object_id integer;
-  v_actor_id integer;
-  v_visited_object_ids integer[];
   v_full_card_function text;
   v_redirect_object_id integer;
   v_object_exists boolean;
@@ -18,89 +17,40 @@ declare
   v_object jsonb;
   v_list jsonb;
 begin
-  assert in_client_id is not null;
-
-  select actor_id
-  into v_actor_id
-  from data.clients
-  where id = in_client_id
-  for update;
-
-  if v_actor_id is null then
-    raise exception 'Client % has no active actor', in_client_id;
-  end if;
-
   select id
   into v_object_id
   from data.objects
   where
     code = v_object_code and
-    type = 'instance'
-  for update;
+    type = 'instance';
 
   if v_object_id is null then
-    perform data.log('error', format('Attempt to subscribe for non-existing object'' changes with code %s. Redirecting to 404.', v_object_code));
-
-    v_object_id := data.get_integer_param('not_found_object_id');
-
-    select true
-    into v_object_exists
-    from data.objects
-    where
-      id = v_object_id and
-      type = 'instance'
-    for update;
-
-    if v_object_exists is null then
-      raise exception 'Object % not found', v_object_id;
-    end if;
+    perform data.log('warning', format('Attempt to subscribe for non-existing object'' changes with code %s. Redirecting to 404.', v_object_code));
+    perform api_utils.create_open_object_action_notification(in_client_id, in_request_id, data.get_object_code(data.get_integer_param('not_found_object_id')));
+    return;
   end if;
 
-  loop
-    if v_visited_object_ids is not null and array_position(v_visited_object_ids, v_object_id) is not null then
-      raise exception 'Redirection cycle detected for object %', v_object_id;
-    end if;
+  -- Вызываем функцию на получение полной карточки объекта, если есть
+  v_full_card_function := json.get_string_opt(data.get_attribute_value(v_object_id, 'full_card_function'), null);
 
-    v_visited_object_ids := array_append(v_visited_object_ids, v_object_id);
+  if v_full_card_function is not null then
+    execute format('select %s($1, $2)', v_full_card_function)
+    using v_object_id, v_actor_id;
+  end if;
 
-    -- Вызываем функцию на получение полной карточки объекта, если есть
-    v_full_card_function := json.get_string_opt(data.get_attribute_value(v_object_id, 'full_card_function'), null);
+  -- Смотрим на наличие redirect'а
+  v_redirect_object_id := json.get_integer_opt(data.get_attribute_value(v_object_id, 'redirect', v_actor_id), null);
+  if v_redirect_object_id is not null then
+    perform api_utils.create_open_object_action_notification(in_client_id, in_request_id, data.get_object_code(v_redirect_object_id));
+    return;
+  end if;
 
-    if v_full_card_function is not null then
-      execute format('select %s($1, $2)', v_full_card_function)
-      using v_object_id, v_actor_id;
-    end if;
-
-    -- Смотрим на наличие redirect'а
-    v_redirect_object_id := json.get_integer_opt(data.get_attribute_value(v_object_id, 'redirect', v_actor_id), null);
-    if v_redirect_object_id is not null then
-      v_object_id := v_redirect_object_id;
-
-      -- Проверяем наличие объекта
-      select true
-      into v_object_exists
-      from data.objects
-      where
-       id = v_object_id and
-       type = 'instance'
-      for update;
-
-      if v_object_exists is null then
-        raise exception 'Object % not found', v_object_id;
-      end if;
-
-      continue;
-    end if;
-
-    -- Проверяем видимость
-    v_is_visible := json.get_boolean_opt(data.get_attribute_value(v_object_id, 'is_visible', v_actor_id), false);
-    if not v_is_visible then
-      v_object_id := data.get_integer_param('not_found_object_id');
-      continue;
-    end if;
-
-    exit;
-  end loop;
+  -- Проверяем видимость
+  v_is_visible := json.get_boolean_opt(data.get_attribute_value(v_object_id, 'is_visible', v_actor_id), false);
+  if not v_is_visible then
+    perform api_utils.create_open_object_action_notification(in_client_id, in_request_id, data.get_object_code(data.get_integer_param('not_found_object_id')));
+    return;
+  end if;
 
   select true
   into v_subscription_exists
