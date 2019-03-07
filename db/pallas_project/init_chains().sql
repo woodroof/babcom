7454553dac9da127d1ff833e7e83b456aa419ee9
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2019-03-08 01:46:15 +0700

    Часть документов

--- db/pallas_project/init_chains().sql
+++ db/pallas_project/init_chains().sql
@@ -9,8 +9,7 @@ begin
   -- todo
   -- оружие, СВП 5f7c2dc0-0cb4-4fc5-870c-c0776272a02e a11d2240-3dce-4d75-bc52-46e98b07ff27 5a764843-9edc-4cfb-8367-80c1d3c54ed9
   -- стимуляторы, картель 18ce44b8-5df9-4c84-8af4-b58b3f5e7b21 70e5db08-df47-4395-9f4a-15eef99b2b89 71efd585-080c-431d-a258-b4e222ff7623
-  -- оружие, картель 70e5db08-df47-4395-9f4a-15eef99b2b89
-  -- алмазы, СВП 5074485d-73cd-4e19-8d4b-4ffedcf1fb5f 3beea660-35a3-431e-b9ae-e2e88e6ac064
+  -- алмазы, СВП 5074485d-73cd-4e19-8d4b-4ffedcf1fb5f 3beea660-35a3-431e-b9ae-e2e88e6ac064 3a83fb3c-b954-4a04-aa6c-7a46d7bf9b8e 37fb2074-498c-4d28-8395-9fdf993f2b06
 end;
 $$
 language plpgsql;
