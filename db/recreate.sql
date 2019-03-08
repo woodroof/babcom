37ef53257c46f561e4d51a6cb517d1ab536c7bd9
Ильиных Данил Сергеевич <d.ilinyh@2gis.ru>
2019-03-08 15:09:19 +0700

    Удаляем маршруты

--- db/recreate.sql
+++ db/recreate.sql
@@ -28867,30 +28867,30 @@ begin
       }
     }');
 
-  perform pallas_project.create_route(
-    'Маршрут',
-    'TODO — Алмазная жила',
-    'TODO описание',
-    '(1,1) (1,2) (2,2)',
-    array['2ce20542-04f1-418f-99eb-3c9d2665f733']);
-  perform pallas_project.create_route(
-    'Маршрут',
-    'TODO — Теко Марс, склад 1',
-    'TODO описание',
-    '(1,1) (1,2) (2,2)',
-    array['1fbcf296-e9ad-43b0-9064-1da3ff6d326d', '8f7b1cc6-28cd-4fb1-8c81-e0ab1c0df5c9', 'ea68988b-b540-4685-aefb-cbd999f4c9ba']);
-  perform pallas_project.create_route(
-    'Маршрут',
-    'TODO — Теко Марс, склад 2',
-    'TODO описание',
-    '(1,1) (1,2) (2,2)',
-    array['1fbcf296-e9ad-43b0-9064-1da3ff6d326d', '8f7b1cc6-28cd-4fb1-8c81-e0ab1c0df5c9', 'ea68988b-b540-4685-aefb-cbd999f4c9ba']);
-  perform pallas_project.create_route(
-    'Маршрут',
-    'TODO — Картель, склад',
-    'TODO описание',
-    '(1,1) (1,2) (2,2)',
-    array['0a0dc809-7bf1-41ee-bfe7-700fd26c1c0a', '1fbcf296-e9ad-43b0-9064-1da3ff6d326d']);
+  --perform pallas_project.create_route(
+  --  'Маршрут',
+  --  'TODO — Алмазная жила',
+  --  'TODO описание',
+  --  '(1,1) (1,2) (2,2)',
+  --  array['2ce20542-04f1-418f-99eb-3c9d2665f733']);
+  --perform pallas_project.create_route(
+  --  'Маршрут',
+  --  'TODO — Теко Марс, склад 1',
+  --  'TODO описание',
+  --  '(1,1) (1,2) (2,2)',
+  --  array['1fbcf296-e9ad-43b0-9064-1da3ff6d326d', '8f7b1cc6-28cd-4fb1-8c81-e0ab1c0df5c9', 'ea68988b-b540-4685-aefb-cbd999f4c9ba']);
+  --perform pallas_project.create_route(
+  --  'Маршрут',
+  --  'TODO — Теко Марс, склад 2',
+  --  'TODO описание',
+  --  '(1,1) (1,2) (2,2)',
+  --  array['1fbcf296-e9ad-43b0-9064-1da3ff6d326d', '8f7b1cc6-28cd-4fb1-8c81-e0ab1c0df5c9', 'ea68988b-b540-4685-aefb-cbd999f4c9ba']);
+  --perform pallas_project.create_route(
+  --  'Маршрут',
+  --  'TODO — Картель, склад',
+  --  'TODO описание',
+  --  '(1,1) (1,2) (2,2)',
+  --  array['0a0dc809-7bf1-41ee-bfe7-700fd26c1c0a', '1fbcf296-e9ad-43b0-9064-1da3ff6d326d']);
 end;
 $$
 language plpgsql;
